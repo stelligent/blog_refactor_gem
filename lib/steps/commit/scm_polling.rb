52ec8e3e2c70ae0ebc659e11c8bcb7c6e81b31eb
Jeffrey Dugas <jedugas@gmail.com>
2016-09-15 08:44:30 -0400

    Add back defaults for some params

--- lib/steps/commit/scm_polling.rb
+++ lib/steps/commit/scm_polling.rb
@@ -9,7 +9,18 @@ module Build
       def initialize(store:)
         params = {}
         %w{app_name repository_url repository_branch working_directory aws_region aws_vpc aws_subnets aws_azs aws_keypair}.each do |var|
-          params[var.to_sym] = ENV[var] || fail(ERROR_MESSAGE % [var])
+          case var
+          when 'aws_region'
+            params[var.to_sym] = ENV[var] || 'us-east-1'
+          when 'aws_vpc'
+            params[var.to_sym] = ENV[var] || 'vpc-857a3ee2'
+          when 'aws_subnets'
+            params[var.to_sym] = ENV[var] || 'subnet-c5a76a8c,subnet-3b233a06'
+          when 'aws_azs'
+            params[var.to_sym] = ENV[var] || 'us-east-1c,us-east-1b'
+          else
+            params[var.to_sym] = ENV[var] || fail(ERROR_MESSAGE % [var])
+          end
         end
 
         FileUtils.rm_r(params[:working_directory]) if File.directory?(params[:working_directory])
