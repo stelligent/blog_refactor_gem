44a05692891c6d67f5bfc07e4ce62275efc8ec69
Jeffrey Dugas <jedugas@gmail.com>
2016-09-15 18:48:08 -0400

    Add additional CFN utility

--- lib/steps/utils/cfn.rb
+++ lib/steps/utils/cfn.rb
@@ -2,14 +2,31 @@ require 'aws-sdk-core'
 
 module BlogRefactorGem
   module Utils
-    module CFN
-      def get_stack(stack_name:, region:)
-        # do stuff
-        cfn_client ||= Aws::CloudFormation::Client.new(region: region)
+    module Cfn
+      def get_stack(region:, name:)
         begin
-          return cfn_client.describe_stacks(stack_name: stack_name).stacks.first
+          return Aws::CloudFormation::Client.new(region: region).describe_stacks(stack_name: name).stacks.first
         rescue Aws::CloudFormation::Errors::ValidationError
           return nil
+        end
+      end
+
+      def put_stack(region:, name:, template_url:, parameters:, tags:)
+        required_options = {
+          stack_name: name,
+          template_url: template_url,
+          parameters: parameters.map { |key, value| { parameter_key: key, parameter_value: value, use_previous_value: false } }.to_a,
+          capabilities: ['CAPABILITY_IAM'],
+          disable_rollback: true
+        }
+        options = tags.nil? ? required_options : { tags: tags }.merge(required_options)
+        cfn = Aws::CloudFormation::Client.new(region: region)
+        cfn.create_stack(options)
+
+        begin
+          cfn.wait_until(:stack_create_complete, stack_name: name)
+        rescue Aws::Waiters::Errors::FailureStateError => error
+          raise error
         end
       end
     end
