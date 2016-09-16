c3ab080a65bcc2279d74a57cac872348afb32ea1
Jeffrey Dugas <jedugas@gmail.com>
2016-09-16 18:19:37 -0400

    Oops... capture3

--- lib/steps/utils/cmd.rb
+++ lib/steps/utils/cmd.rb
@@ -4,7 +4,7 @@ module BlogRefactorGem
   module Utils
     module Cmd
       def Cmd.execute_shell(command:)
-        stdout, stderr, status = Open3.popen(command)
+        stdout, stderr, status = Open3.capture3(command)
         fail(stderr.readlines.join('')) unless status.exitstatus == 0
         stdout.readlines.join('')
       end
