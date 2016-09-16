041c5f57093e6f04e132a4d6e77f60deec1b6718
Jeffrey Dugas <jedugas@gmail.com>
2016-09-16 10:38:26 -0400

    enforce inheritance on EnvironmentConfiguration

--- lib/steps/acceptance/environment_configuration.rb
+++ lib/steps/acceptance/environment_configuration.rb
@@ -1,19 +1,10 @@
-
-# rds creation first
-#   will inject custom attribs into the store about the RDS instance
-# chef json creation
-#   read the custom store attribs
-#   blend in the standard store attribs
-#   make the json
-#   push it into S3
-#
-
 module Build
   module Acceptance
     # Caller MUST override this step!
     class EnvironmentConfiguration
       def initialize(store:)
-        # do nothing special
+        descendants = ObjectSpace.each_object(Class).select { |klass| klass < self }
+        fail unless descendants.any?
       end
     end
   end
