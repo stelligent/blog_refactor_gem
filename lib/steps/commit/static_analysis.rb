717bd087331f2e4d169cdf2af3a7f596f7e5ca7f
Jeffrey Dugas <jedugas@gmail.com>
2016-09-16 18:36:35 -0400

    Fix string interpolation for new execute_shell

--- lib/steps/commit/static_analysis.rb
+++ lib/steps/commit/static_analysis.rb
@@ -17,7 +17,7 @@ module Build
         Dir.chdir(working_directory) do
           puts "Running foodcritic on pipelines/cookbooks/#{app_name}..."
           output = BlogRefactorGem::Utils::Cmd.execute_shell(
-            command: 'foodcritic -t ~FC001 "pipelines/cookbooks/#{app_name}" -P'
+            command: "foodcritic -t ~FC001 'pipelines/cookbooks/#{app_name}' -P"
           )
           puts output
         end
