2cc5591f06c1e82abad14f91896a3e204640feec
Jeffrey Dugas <jedugas@gmail.com>
2016-09-16 18:23:20 -0400

    Oops stdout is a string, not an array

--- lib/steps/utils/cmd.rb
+++ lib/steps/utils/cmd.rb
@@ -6,7 +6,7 @@ module BlogRefactorGem
       def Cmd.execute_shell(command:)
         stdout, stderr, status = Open3.capture3(command)
         fail(stderr.readlines.join('')) unless status.exitstatus == 0
-        stdout.readlines.join('')
+        stdout
       end
     end
   end
